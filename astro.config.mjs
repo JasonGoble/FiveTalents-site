c0618399f7227246c9475d84932fe1039bf89791
Jason Goble <jason@goble.me>
2026-06-02 09:22:14 -0400

    Change site URL to fivetalents.dev
    Updated site URL and removed base path configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jasongoble.github.io',
-  base: '/FiveTalents-site/',
+  site: 'https://fivetalents.dev'
 });
